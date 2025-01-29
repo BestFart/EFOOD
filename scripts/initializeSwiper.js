224a1bafe492fedf25971cd5d54fe8a0521baea7
BestFart <157964493+BestFart@users.noreply.github.com>
2025-01-29 20:25:11 +0300

    Add files via upload

--- scripts/initializeSwiper.js
+++ scripts/initializeSwiper.js
@@ -1,7 +1,7 @@
 export function initializeSwiper() {
     const swiper = new Swiper('.swiper', {
         slidesPerView: 5,
-        spaceBetween: 30,
+        spaceBetween: 46,
         loop: true,
         navigation: {
             nextEl: '.swiper-button-next',
