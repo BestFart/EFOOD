8a4510039789289c530e1ef599a8432273a4e09a
BestFart <157964493+BestFart@users.noreply.github.com>
2025-01-30 16:59:44 +0300

    Add files via upload

--- scripts/search.js
+++ scripts/search.js
@@ -1,16 +1,5 @@
 export function search() {
-    document.addEventListener('DOMContentLoaded', function () {
-        const searchButton = document.querySelector('.search__btn');
-        const searchInput = document.querySelector('.form__input');
-
-        searchButton.addEventListener('click', function () {
-            if (searchInput.style.display === 'none' || searchInput.style.display === '') {
-                searchInput.style.display = 'block';
-                searchInput.focus();
-            } else {
-                searchInput.style.display = 'none';
-            }
-        });
-    });
+   document.querySelector(".search__btn").addEventListener("click", function () {
+    document.querySelector(".search-box").classList.toggle("active");
+  });
 }
-
