f950c99cd087afa41c6df8eaa7a0c13b59731de2
BestFart <157964493+BestFart@users.noreply.github.com>
2025-02-08 11:18:05 +0300

    Add files via upload

--- scripts/initializeSwiper.js
+++ scripts/initializeSwiper.js
@@ -1,12 +1,26 @@
 export function initializeSwiper() {
     const swiper = new Swiper('.swiper', {
-        slidesPerView: 5,
+        slidesPerView: 1,
         spaceBetween: 46,
         loop: true,
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+        breakpoints: {
+            // 640: {
+            //   slidesPerView: 2,
+            //   spaceBetween: 20,
+            // },
+            920: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 5,
+              spaceBetween: 46,
+            },
+          },
     });
 }
 
--- scripts/renderCategorys.js
+++ scripts/renderCategorys.js
@@ -15,18 +15,7 @@ export function renderCategorys() {
       const categoryHTML = ` <div class="swiper-slide">
                 <article class="product" data-id="${item.id}">
                   <img src="images/${item.imgSrc}" alt="burger" class="product-image"/>
-                  <div class="product-rating">
-                    <input type="radio" id="radio1" name="rating"/>
-                    <label for="radio1"></label>
-                    <input type="radio" id="radio2" name="rating"/>
-                    <label for="radio2"></label>
-                    <input type="radio" id="radio3" name="rating"/>
-                    <label for="radio3"></label>
-                    <input type="radio" id="radio4" name="rating"/>
-                    <label for="radio4"></label>
-                    <input type="radio" id="radio5" name="rating"/>
-                    <label for="radio5"></label>
-                  </div>
+                  <div class="product-rating">${item.rating}</div>          
                   <h4 class="product-title">${item.title}</h4>
                   <p class="product-price">$${item.price}</p>
                   <button class="product-add" type="button">${item.add}</button>
